7eb75e1c3d52058199fc88e3a5c3d90074e27e3f
let1ci4b <leticia_b_souza@estudante.sc.senai.br>
2023-06-20 17:22:29 -0300

    botoes posicionados

--- app/src/main/java/com/example/calculadora/Main.kt
+++ app/src/main/java/com/example/calculadora/Main.kt
@@ -1,4 +1,16 @@
 package com.example.calculadora
 
-class Main {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.calculadora.databinding.MainlayoutBinding
+
+class Main : ComponentActivity() {
+
+    private lateinit var binding: MainlayoutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = MainlayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
