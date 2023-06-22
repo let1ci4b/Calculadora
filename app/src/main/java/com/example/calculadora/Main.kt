5e0832ae34514b86c0d161bada7256200e387f0b
let1ci4b <leticia_b_souza@estudante.sc.senai.br>
2023-06-22 17:18:20 -0300

    new commit

--- app/src/main/java/com/example/calculadora/Main.kt
+++ app/src/main/java/com/example/calculadora/Main.kt
@@ -12,5 +12,38 @@ class Main : ComponentActivity() {
         super.onCreate(savedInstanceState)
         binding = MainlayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        onClick()
+    }
+
+    private fun onClick(){
+        with(binding){
+
+            buttonZero.setOnClickListener() { insert(0) }
+            buttonOne.setOnClickListener() { insert(1) }
+            buttonTwo.setOnClickListener() { insert(2) }
+            buttonThree.setOnClickListener() { insert(3) }
+            buttonFour.setOnClickListener() { insert(4) }
+            buttonFive.setOnClickListener() { insert(5) }
+            buttonSix.setOnClickListener() { insert(6) }
+            buttonSeven.setOnClickListener() { insert(7) }
+            buttonEight.setOnClickListener() { insert(8) }
+            buttonNine.setOnClickListener() { insert(9) }
+        }
+    }
+
+    private fun clear(){
+
+    }
+
+    private fun insert(num: Int){
+
+    }
+
+    private fun backspace(){
+
+    }
+
+    private fun calculate(){
+
     }
 }
